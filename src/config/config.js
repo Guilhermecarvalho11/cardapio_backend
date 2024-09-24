e1f3ccf18346acf1ebb04ffe83d72a3bc31863a9
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-09-23 21:10:51 -0300

    fix bug

--- src/config/config.js
+++ src/config/config.js
@@ -2,50 +2,56 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_DEVELOPMENT,
-    host: process.env.DB_HOST,
-    dialect: "postgres", // Alterado para PostgreSQL
+    username: process.env.DB_USER || "dbcardapio_m6ph_user",
+    password: process.env.DB_PASSWORD || "tGy9w3cmuFx9Rg2Vv1zQcNwKIKzYomt8",
+    database: process.env.DB_NAME_DEVELOPMENT || "dbcardapio_m6ph",
+    host:
+      process.env.DB_HOST ||
+      "dpg-crovci08fa8c73dq4570-a.oregon-postgres.render.com",
+    dialect: process.env.DB_DIALECT || "postgres",
+    port: process.env.DB_PORT || 5432,
     pool: {
       max: 10,
       min: 0,
       acquire: 30000,
       idle: 10000,
     },
-    port: process.env.DB_PORT || 5432,
     dialectOptions: {
       ssl: {
-        require: true, // Habilita a conexão SSL
-        rejectUnauthorized: false, // Pode ser definido como true se você tiver um certificado
+        require: true,
+        rejectUnauthorized: false,
       },
     },
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
-    host: process.env.DB_HOST,
-    dialect: "postgres", // Alterado para PostgreSQL
+    username: process.env.DB_USER || "dbcardapio_m6ph_user",
+    password: process.env.DB_PASSWORD || "tGy9w3cmuFx9Rg2Vv1zQcNwKIKzYomt8",
+    database: process.env.DB_NAME_TEST || "dbcardapio_m6ph_test",
+    host:
+      process.env.DB_HOST ||
+      "dpg-crovci08fa8c73dq4570-a.oregon-postgres.render.com",
+    dialect: process.env.DB_DIALECT || "postgres",
     port: process.env.DB_PORT || 5432,
     dialectOptions: {
       ssl: {
-        require: true, // Habilita a conexão SSL
-        rejectUnauthorized: false, // Pode ser definido como true se você tiver um certificado
+        require: true,
+        rejectUnauthorized: false,
       },
     },
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_PRODUCTION,
-    host: process.env.DB_HOST,
-    dialect: "postgres", // Alterado para PostgreSQL
+    username: process.env.DB_USER || "dbcardapio_m6ph_user",
+    password: process.env.DB_PASSWORD || "tGy9w3cmuFx9Rg2Vv1zQcNwKIKzYomt8",
+    database: process.env.DB_NAME_PRODUCTION || "dbcardapio_m6ph_prod",
+    host:
+      process.env.DB_HOST ||
+      "dpg-crovci08fa8c73dq4570-a.oregon-postgres.render.com",
+    dialect: process.env.DB_DIALECT || "postgres",
     port: process.env.DB_PORT || 5432,
     dialectOptions: {
       ssl: {
-        require: true, // Habilita a conexão SSL
-        rejectUnauthorized: false, // Pode ser definido como true se você tiver um certificado
+        require: true,
+        rejectUnauthorized: false,
       },
     },
   },
